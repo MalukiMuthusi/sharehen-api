4b912ef9cacfdd361bf55a8914d1466897dd6eb6
Maluki Muthusi <malukimuthusi@gmail.com>
2021-06-26 19:42:43 +0300

    Add an api design document

---
+++ api_design.md
@@ -0,0 +1,48 @@
+# High level API design
+
+```txt
+Ride-owner
+- id
+- car-id
+- name
+- car-type
+- space available
+- journey id
+```
+
+```txt
+Ride-owner-broadcast
+- rider-id
+- fee
+- journey
+```
+
+```txt
+Rider
+- id
+- name
+- space needed
+- journey id
+```
+
+```txt
+Rider-request
+- id
+- rider-id
+```
+
+```txt
+Journey
+- start time
+- end point
+- start point
+- locations
+```
+
+```txt
+car
+- id
+- color
+- model
+- description
+```
--- data/rides.go
+++ data/rides.go
@@ -5,40 +5,40 @@ package data
 
 // Rider requests for rides
 type Rider struct {
-	Id   *string
-	Name *string
+	Id   *string `json:"id,omitempty"`
+	Name *string `json:"name,omitempty"`
 }
 
 // Ride owner volunteers to share ride
 type RideOwner struct {
-	Id    *string
-	Name  *string
-	CarId *string
+	Id    *string `json:"id,omitempty"`
+	Name  *string `json:"name,omitempty"`
+	CarId *string `json:"car_id,omitempty"`
 }
 
 // RideOwner volunteers and asks for riders
 type RideOwnerOffer struct {
-	Id          *string
-	OfferAmount *FeeType
-	RideOwnerId *string
+	Id          *string  `json:"id,omitempty"`
+	OfferAmount *FeeType `json:"offer_amount,omitempty"`
+	RideOwnerId *string  `json:"ride_owner_id,omitempty"`
 }
 
 // A ride is shared by a rideOwner and a rider
 type Ride struct {
-	Id        *string    `json:"ID"`
-	Rider     *Rider     `json:"rider"`
+	Id        *string    `json:"id,omitempty"`
+	Rider     *Rider     `json:"rider,omitempty"`
 	RideOwner *RideOwner `json:"ride_owner"`
 }
 
 // The request made by a rider for a ride
 type RiderRequest struct {
-	Id          *string
-	RiderId     *string
-	OfferAmount *FeeType
+	Id          *string  `json:"id,omitempty"`
+	RiderId     *string  `json:"rider_ide,omitempty"`
+	OfferAmount *FeeType `json:"offer_amount,omitempty"`
 }
 
 // the currency and the amount
 type FeeType struct {
-	Currency *string
-	Amount   *float32
+	Currency *string  `json:"currency,omitempty"`
+	Amount   *float32 `json:"amount,omitempty"`
 }
